5099fcac3a90e0b8dd89e3f30f52a20ee16b4771
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-01-06 18:54:45 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,8 @@
+function addUser() {
+    user_name = document.getElementById("user_name").nodeValue;
+
+    localStorage.setItem("user_name", user_name);
+
+        window.localtion = "Kwitter_room.html";
+}
+
